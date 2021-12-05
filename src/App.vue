0a8231955290baea2a30212f9081911cffb47676
mirzaak <mirzakaric002@gmail.com>
2021-12-05 07:14:20 +0100

    Bez dropdown on user

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="nav">
+    <div class="lijevo">
+      <router-link to="/"><img src="./assets/logo.svg" alt="logo" class="logo"></router-link>
+      <div class="dropdown">
+        <a href="#" class="naslovi" id="movies">Movies</a>
+          <ul class="moviesdropdown">
+            <li>Popular</li>
+            <li>Now playing</li>
+            <li>Upcoming</li>
+            <li>Top rated</li>
+          </ul>
+      </div>
+        <a href="#" class="naslovi">TV Shows</a>
+        <a href="#" class="naslovi">People</a>
+    </div>
+    <div class="desno">
+        <a href="#" class="naslovi">Login</a>
+        <a href="#" class="naslovi">Sign Up</a>
+        <a href="#" class="naslovi">Search</a>
+    </div>
   </div>
   <router-view/>
 </template>
+
+<script>
+export default{
+data(){
+  return{
+    
+  }
+},
+methods:{
+
+}
+}
+</script>
 
 <style>
 #app {
@@ -14,17 +44,55 @@
   text-align: center;
   color: #2c3e50;
 }
+body{
+  margin: 0;
+}
+.nav{
+  background:#0d253f;
+  height: 50px;
+  width:100%;
+  display: flex;
+  
+}
+.lijevo{
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-left: 100px;
+}
+.desno{
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-left: auto;
+  margin-right: 100px;
+  
+}
+.logo{
+  height: 15px;
+  margin-right: 20px;
 
-#nav {
-  padding: 30px;
+}
+.naslovi{
+  color: white;
+  text-decoration: none;
+  font-size: 0.9em;
+  margin: 0 20px;
+  
+}
+.moviesdropdown{
+  display: none;
+  position: absolute;
+  background: white;
+  list-style-type: none;
+  text-align: left;
+  padding: 5px;
+  margin:0;
+  border-radius: 10px;
+  cursor: pointer;
+}
+.dropdown:hover .moviesdropdown{
+  display: block;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
